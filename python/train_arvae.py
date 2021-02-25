98c13ff3d38128b5e8d43c8a14ca8664498a7c1b
Louise Xu <louisexu@amazon.com>
2021-02-25 19:41:49 +0000

    Add data processing functions and edit plotting functions

--- python/train_arvae.py
+++ python/train_arvae.py
@@ -8,10 +8,7 @@ import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TestTubeLogger
 
-<<<<<<< HEAD
-=======
 from typing import Optional
->>>>>>> 588be1c43e29e166a4aaaf71ed881cbdb197d412
 
 sns.set()
 
@@ -118,207 +115,139 @@ def intervene_raw(target_idx, feature_idx, bias, intervention, checkpoint_path, 
 
 
 # Function to generate example samples
-def generate_example_sample(data, vae_model, post_intervention_vae_model=None, T0=None, eps=None):
-    # DATA -> PI -> Z -> W -> S(prime) ###
-    pi = vae_model.encode(torch.tensor(data).float())  # calculate vector in sampling space; pi = (mean, log variance)
-    #print(pi[0][0])
-    if eps is None:
-        mu, logvar = pi
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-    Z = vae_model.reparameterize(pi, eps)  # drawing sample from the sampling space
-    #print(Z.shape)
-    S_init = torch.tensor(data[0:vae_model.lag, :, :]).float()  # initializing lag terms
+def generate_example_sample(vae_model, targets, adjust, T, B, N, latent_values=None):
 
-    T = Z.shape[0]  # length of timeseries
-    B = Z.shape[1]  # number of bundles
-    S = S_init
+    latent_dim = vae_model.latent_dim
 
-    # Hardcoding few dimensions in latent space
-    peaks = vae_model.make_peak(T, B)
-    trend = vae_model.make_trend(T, B, 0.005)
-    seasonality_y = vae_model.make_seasonality(T, B, 365)
-    seasonality_m = vae_model.make_seasonality(T, B, 30)
-    w1 = torch.cat((peaks, trend, seasonality_y, seasonality_m), axis=2)
+    if latent_values is not None:
+        nz = latent_values
+    else:
+        nz = (torch.rand(1,B,latent_dim) * 4) - 2
 
-    # LOOPING OVER EACH STEP
-    for t in range(T):
-        # z = torch.cat([Z[t]] + [S[-i] for i in range(vae_model.lag)], dim=1)
-        z = Z[t]
-        result = vae_model.decoder_input(z)  # calculating input to decoder
-        #print(result.shape)
-        w = vae_model.decoder(result)  # calculating latent vector
-        #print(w.shape)
-        # concat latent vector with addition trends (seasonality+peaks+linear)
-        w_appended = torch.cat((w, w1[t, :, :]), axis=1)
-        w_lag = torch.cat([w] + [S[-i] for i in range(vae_model.lag)], dim=1)
-        #print(w_lag.shape)
-        # POST INTERVENTION PART
-        if((post_intervention_vae_model is not None) and (T0 is not None)):
-            if t >= (T0-vae_model.lag):
-                if t == (T0-vae_model.lag):
-                    Sprime = S.clone()  # copy the pre-intervention model
-                sprime = post_intervention_vae_model.final_layer(w_lag)  # calculate the outcome
-                # collect the outcome
-                Sprime = torch.cat([Sprime, sprime.reshape([1, sprime.shape[0], sprime.shape[1]])])
+    s = vae_model.decode(nz).detach().numpy()
+    s = s.squeeze(axis=0)
+    targets_var = rescale(s, targets, adjust, T, B, N)
 
-        # PRE INTERVENTION PART/ COUNTERFACTUAL
-        s = vae_model.final_layer(w_lag)
-        S = torch.cat([S, s.reshape([1, s.shape[0], s.shape[1]])])
+    neg_adj = np.min(targets_var,axis=0)
+    neg_adj[neg_adj>0] = 0
+    targets_var = targets_var - neg_adj
 
-        # COLLECTING LATENT VECTORS
-        if t == 0:
-            W = w.reshape([1, w.shape[0], w.shape[1]])
-        else:
-            W = torch.cat([W, w.reshape([1, w.shape[0], w.shape[1]])])
-    #print(W.shape)
-    if((post_intervention_vae_model is not None) and (T0 is not None)):
-        return S, Sprime, T0, pi, Z, W
-    return S, pi, Z, W
+    return targets_var
 
 
 # Function to plot data+samples
-def plot(data, vae_model, post_intervention_vae_model=None, T0=None, out_folder=None):
+def plot_data(vae_model, targets, adjust, T, N, B=6, latent_values=None):
+    # Need to add functionality for ploting post_intervention_vae_model
     sns.set(font_scale=1.25)
 
-    if((post_intervention_vae_model is not None) and (T0 is not None)):
-        S, Sprime, T0, pi, Z, W = generate_example_sample(data, vae_model,
-                                                          post_intervention_vae_model=post_intervention_vae_model,
-                                                          T0=T0)
-    else:
-        S, pi, Z, W = generate_example_sample(data, vae_model)
-    mu, logvar = pi
-    T = mu.shape[0]
-    for i in range(data.shape[1]):
-        fig, ax = plt.subplots(nrows=3, ncols=2, figsize=(35, 20))
+    targets_var = generate_example_sample(vae_model, targets, adjust, T, B, N, latent_values=latent_values)
 
-        ax[0, 0].plot(np.arange(vae_model.lag + 5, T+vae_model.lag), (S).detach().numpy()[vae_model.lag + 5:, i, :], alpha=0.5)
-        ax[0, 0].set_ylabel('Normalized (Outcome)')
-        ax[0, 0].set_xlabel('Time Steps')
-        ax[0, 0].title.set_text('Simulated X')
-
-        ax[0, 1].plot(np.arange(vae_model.lag + 5, T+vae_model.lag), data[vae_model.lag + 5:, i, :])
-        ax[0, 1].set_ylabel('Normalized (Outcome)')
-        ax[0, 1].set_xlabel('Time Steps')
-        ax[0, 1].title.set_text('Observed X')
-
-        ax[1, 0].plot(np.arange(vae_model.lag, vae_model.lag+T), (mu).detach().numpy()[:, i, :])
-        ax[1, 0].set_ylabel('Latent Value')
-        ax[1, 0].set_xlabel('Time Steps')
-        ax[1, 0].title.set_text('Mean Z')
-
-        ax[1, 1].plot(np.arange(vae_model.lag, vae_model.lag+T), (logvar).detach().numpy()[:, i, :])
-        ax[1, 1].set_ylabel('Latent Value')
-        ax[1, 1].set_xlabel('Time Steps')
-        ax[1, 1].title.set_text('Log-Std Z')
-
-        ax[2, 0].plot(np.arange(0, T+vae_model.lag),
-                      (S).detach().numpy()[:, i, :]-(S).detach().numpy()[:, i, :], alpha=0.5)
-        ax[2, 0].set_ylabel('Normalized (Outcome)')
-        ax[2, 0].set_xlabel('Time Steps')
-        ax[2, 0].title.set_text('Difference: X(Intervened) - X')
-
-        ax[2, 1].plot(np.arange(vae_model.lag, T+vae_model.lag), (W).detach().numpy()[:, i, :])
-        ax[2, 1].set_ylabel('Outcome (Penultimate Layer)')
-        ax[2, 1].set_xlabel('Time Steps')
-        ax[2, 1].title.set_text('Simulated W')
-
-        if post_intervention_vae_model is not None:
-            if T0 is not None:
-                ax[0, 0].set_prop_cycle(None)
-                ax[0, 0].plot(np.arange(0, T+post_intervention_vae_model.lag),
-                              (Sprime).detach().numpy()[:, i, :])
-                ax[0, 0].axvline(x=T0)
-                ax[0, 0].set_prop_cycle(None)
-                ax[2, 0].plot(np.arange(0, T+post_intervention_vae_model.lag),
-                              (Sprime).detach().numpy()[:, i, :]-(S).detach().numpy()[:, i, :])
-                ax[2, 0].axvline(x=T0)
-
-        if out_folder is not None:
-<<<<<<< HEAD
-            fig.savefig(out_folder+'example_sample_%d.png' % (i))
-=======
-            fig.savefig(out_folder + "example_sample_%d.png" % (i))
+    fig, ax = plt.subplots(nrows=3, ncols=2, figsize=(35, 20))        
+        
+    ax[0, 0].plot(np.arange(np.shape(targets_var)[0]), targets_var[:,0,:], alpha=0.5)
+    ax[0, 1].plot(np.arange(np.shape(targets_var)[0]), targets_var[:,1,:], alpha=0.5)
+    ax[1, 0].plot(np.arange(np.shape(targets_var)[0]), targets_var[:,2,:], alpha=0.5)
+    ax[1, 1].plot(np.arange(np.shape(targets_var)[0]), targets_var[:,3,:], alpha=0.5)
+    ax[2, 0].plot(np.arange(np.shape(targets_var)[0]), targets_var[:,4,:], alpha=0.5)
+    ax[2, 1].plot(np.arange(np.shape(targets_var)[0]), targets_var[:,5,:], alpha=0.5)
+    plt.show()
+    plt.close()
 
 
-# Exclude Bundles with small targets
-data = np.delete(data, np.argwhere(median_targets[:, :2] < 200)[:, 0], 1)
-mean_targets = np.delete(
-    mean_targets, np.argwhere(median_targets[:, :2] < 200)[:, 0], 0
-)
+def plot_latent_space(vae_model, data):
+    sns.set(font_scale=1.25)
 
+    _, _, mu, log_var = vae_model.forward(torch.tensor(data).float())
+    latent_dim = vae_model.latent_dim
+    fig, ax = plt.subplots(nrows=latent_dim, ncols=2, figsize=(35, 20)) 
+    for i in range(latent_dim):
+        ax[i,0].hist(mu[0,:,i].detach().numpy())
+        ax[i,1].hist(np.exp(log_var[0,:,i].detach().numpy()))
+    plt.show()
 
-class DataProcesser:
-    """
-    """
+def prepare_input(data, targets, adjust: Optional[float] = 10, outlier_threshold: Optional[float]=5):
 
-    def __init__(self, data: np.ndarray, targets: int, adjust: Optional[float] = 10):
-        B = data.shape[1]  # total number of bundles
-        T = data.shape[0]  # length timeseries
-        N = data.shape[2]  # total number of donors + targets
+    data_norm0, std_t, std_d = normalized_data(data, targets)
 
-        self.B = B
-        self.T = T
-        self.N = N
-        self.targets = targets
-        self.adjust = adjust
+    # remove outlier
+    if outlier_threshold <=0:
+        raise ValueError(
+            "Outlier threshold must be positive."
+        )
+    data_norm0[data_norm0 > outlier_threshold] = outlier_threshold
+    data_norm0[data_norm0 < -outlier_threshold] = -outlier_threshold
 
-        def prepare_input(self, data: np.ndarray):
-            data_norm0 = self.normalized_data(data=data)
+    # convert 3D to 2D
+    data_norm = convert_to_2d(data_norm0)
 
-            # remove outlier
-            data_norm0[data_norm0 > 5] = 5
-            data_norm0[data_norm0 < -5] = -5
+    # append mean and std
+    mean_targets = np.mean(data[:, :, :], axis=0)
 
-            # convert 3D to 2D
-            data_norm, std_t, std_d = convert_to_2d(self, data_norm0)
+    processed_data_norm = np.append(
+        np.append(
+            np.append(data_norm, np.log(std_t[:, None]) / adjust, axis=1),
+            np.log(std_d[:, None]) / adjust,
+            axis=1,
+        ),
+        np.log(mean_targets) / adjust,
+        axis=1,
+    )
+    return processed_data_norm
 
-            # append mean and std
-            mean_targets = np.mean(data[:, :, :], axis=0)
+def normalized_data(data: np.ndarray, targets):
+    N = data.shape[2]  # total number of donors + targets
+    
+    # Separetly compute standard deviation for largest target and largest donor
+    std_t = np.std(data[:, :, targets - 1], axis=0)
+    std_d = np.std(data[:, :, N - 1], axis=0)
 
-            processed_data_norm = np.append(
-                np.append(
-                    np.append(data_norm, np.log(std_t[:, None]) / adjust, axis=1),
-                    np.log(std_d[:, None]) / adjust,
-                    axis=1,
-                ),
-                np.log(mean_targets) / adjust,
-                axis=1,
-            )
-            return processed_data_norm
+    data_norm0t = (
+        data[:, :, :targets] - np.mean(data[:, :, :targets], axis=0)
+    ) / std_t[None, :, None]
+    data_norm0d = (
+        data[:, :, targets:] - np.mean(data[:, :, targets:], axis=0)
+    ) / std_d[None, :, None]
+    data_norm0 = np.append(data_norm0t, data_norm0d, axis=2)
 
-        def normalized_data(self, data: np.ndarray):
-            N = self.N
-            targets = self.targets
+    print(f"Shape: {data_norm0.shape}")
+    print(f"\nMean of raw data: {np.mean(data):.3f}")
+    print(f"Std. dev of raw data: {np.std(data):.3f}")
+    print(f"\nMean of normalized data: {np.mean(data_norm0):.3f}")
+    print(f"Std. dev of normalized data: {np.std(data_norm0):.3f}")
+    return data_norm0, std_t, std_d
 
-            # Separetly compute standard deviation for largest target and largest donor
-            std_t = np.std(data[:, :, targets - 1], axis=0)
-            std_d = np.std(data[:, :, N - 1], axis=0)
+def convert_to_2d(data_input):
+    B = data_input.shape[1]  # total number of bundles
+    T = data_input.shape[0]  # length timeseries
+    N = data_input.shape[2]  # total number of donors + targsets
 
-            data_norm0t = (
-                data[:, :, :targets] - np.mean(data[:, :, :targets], axis=0)
-            ) / std_t[None, :, None]
-            data_norm0d = (
-                data[:, :, targets:] - np.mean(data[:, :, targets:], axis=0)
-            ) / std_d[None, :, None]
-            data_norm0 = np.append(data_norm0t, data_norm0d, axis=2)
+    data_rescale = np.zeros((B, N * T))
+    for i in range(0, B):
+        for j in range(0, N):
+            data_rescale[i, T * j : T * (j + 1)] = data_input[:, i, j]
+    return data_rescale
 
-            print(f"Shape: {data_norm0.shape}")
-            print(f"\nMean of raw data: {np.mean(data):.3f}")
-            print(f"Std. dev of raw data: {np.std(data):.3f}")
-            print(f"\nMean of normalized data: {np.mean(data_norm0):.3f}")
-            print(f"Std. dev of normalized data: {np.std(data_norm0):.3f}")
-            return data_norm0, std_t, std_d
+def convert_to_3d(data_input, T, B, N):
+    data_input_3d = np.zeros((T,B, N))
+    for i in range(0,B):
+        for j in range(0,N):
+            data_input_3d[:,i,j] = data_input[i,T*j:T*(j+1)]
+    return data_input_3d
 
-        def convert_to_2d(self, data_input):
-            B = self.B
-            N = self.N
-            T = self.T
+def rescale(data, targets, adjust, T, B, N):
+    ## rescale function currently only works for 1 level hierarchy
+    data_3d = convert_to_3d(data, T, B, N)
 
-            data_rescale = np.zeros((B, N * T))
-            for i in range(0, B):
-                for j in range(0, N):
-                    data_rescale[i, T * j : T * (j + 1)] = data_input[:, i, j]
-            return data_rescale
+    normalizers = np.exp(data[:,-(N+targets):]*adjust)
+    data_rescaled = np.append(data_3d[:,:,:targets]*normalizers[:,0,None],data_3d[:,:,targets:]*normalizers[:,1,None],axis=2) + normalizers[:,targets:]
+    return data_rescaled
 
->>>>>>> 588be1c43e29e166a4aaaf71ed881cbdb197d412
+    
+def exclude_small_targets(data: np.ndarray, targets: int, threshold: Optional[float] = 200):
+    median_targets = np.median(data[:, :, :], axis=0)
+    # Exclude Bundles with small targets
+    data = np.delete(
+        data, np.argwhere(median_targets[:, :targets] < threshold)[:, 0], 1
+    )
+    return data
+
