1bdc226ec943ae0f091f0511a38a12d4d58a1620
Louise Xu <louisexu@amazon.com>
2021-02-24 17:32:31 -0800

    update train_arvae.py

--- python/train_arvae.py
+++ python/train_arvae.py
@@ -11,51 +11,59 @@ from pytorch_lightning.loggers import TestTubeLogger
 sns.set()
 
 
-def train(data, hyper_params, input_checkpoint_path=None, output_checkpoint_path='ar_vae.ckpt', M_N=1):
+def train(
+    data,
+    hyper_params,
+    input_checkpoint_path=None,
+    output_checkpoint_path="ar_vae.ckpt",
+    M_N=1,
+):
     # TRAINING FUNCTION
-    max_epochs = hyper_params['epochs']
-    lag = hyper_params['lag']
-    latent_dim = hyper_params['latent_dim']
-    hidden_dims = hyper_params['hidden_dims']
+    max_epochs = hyper_params["epochs"]
+    lag = hyper_params["lag"]
+    latent_dim = hyper_params["latent_dim"]
+    hidden_dims = hyper_params["hidden_dims"]
 
-    vae_model = t_VAE.AR_VAE(lag=lag,
-                             latent_dim=latent_dim,
-                             X=torch.tensor(data).float(),
-                             hidden_dims=hidden_dims,
-                             ).float()
+    vae_model = t_VAE.AR_VAE(
+        lag=lag,
+        latent_dim=latent_dim,
+        X=torch.tensor(data).float(),
+        hidden_dims=hidden_dims,
+    ).float()
     if input_checkpoint_path is not None:
-        vae_model = t_VAE.AR_VAE.load_from_checkpoint(input_checkpoint_path,
-                                                      lag=lag,
-                                                      latent_dim=latent_dim,
-                                                      X=torch.tensor(data).float(),
-                                                      hidden_dims=hidden_dims,
-                                                      ).float()
+        vae_model = t_VAE.AR_VAE.load_from_checkpoint(
+            input_checkpoint_path,
+            lag=lag,
+            latent_dim=latent_dim,
+            X=torch.tensor(data).float(),
+            hidden_dims=hidden_dims,
+        ).float()
 
-    print('Loss Before Training')
+    print("Loss Before Training")
     res = vae_model.forward(torch.tensor(data).float())
     print(vae_model.loss_function(*res, M_N=1))
 
     # Logger
     tt_logger = TestTubeLogger(
-        save_dir=os.getcwd(),
-        name='t_VAE_log',
-        debug=False,
-        create_git_tag=False)
+        save_dir=os.getcwd(), name="t_VAE_log", debug=False, create_git_tag=False
+    )
 
     # Trainer
-    runner = Trainer(max_epochs=max_epochs,
-                     logger=tt_logger,
-                     log_save_interval=50,
-                     train_percent_check=1.,
-                     val_percent_check=1.,
-                     num_sanity_val_steps=100,
-                     early_stop_callback=False)
+    runner = Trainer(
+        max_epochs=max_epochs,
+        logger=tt_logger,
+        log_every_n_steps=50,
+        limit_train_batches=1.0,
+        limit_val_batches=1.0,
+        num_sanity_val_steps=100,
+        checkpoint_callback=False,
+    )
 
     runner.fit(vae_model)
 
     runner.save_checkpoint(output_checkpoint_path)
 
-    print('Loss After Training')
+    print("Loss After Training")
     res = vae_model.forward(torch.tensor(data).float())
     print(vae_model.loss_function(*res, M_N=M_N))
 
@@ -77,16 +85,19 @@ def fetch_ITM(vae_model):
 
 
 # Function to encode Interventions
-def intervene_raw(target_idx, feature_idx, bias, intervention, checkpoint_path, hyper_params, data):
-    lag = hyper_params['lag']
-    latent_dim = hyper_params['latent_dim']
-    hidden_dims = hyper_params['hidden_dims']
-    vae_model_intv = t_VAE.AR_VAE.load_from_checkpoint(checkpoint_path,
-                                                       lag=lag,
-                                                       latent_dim=latent_dim,
-                                                       hidden_dims=hidden_dims,
-                                                       X=torch.tensor(data).float(),
-                                                       ).float()
+def intervene_raw(
+    target_idx, feature_idx, bias, intervention, checkpoint_path, hyper_params, data
+):
+    lag = hyper_params["lag"]
+    latent_dim = hyper_params["latent_dim"]
+    hidden_dims = hyper_params["hidden_dims"]
+    vae_model_intv = t_VAE.AR_VAE.load_from_checkpoint(
+        checkpoint_path,
+        lag=lag,
+        latent_dim=latent_dim,
+        hidden_dims=hidden_dims,
+        X=torch.tensor(data).float(),
+    ).float()
     w_i, b_i = fetch_ITM(vae_model_intv)
     print(w_i.shape)
     print(w_i[target_idx, :][:, feature_idx])
@@ -109,16 +120,22 @@ def intervene_raw(target_idx, feature_idx, bias, intervention, checkpoint_path, 
 
 
 # Function to generate example samples
-def generate_example_sample(data, vae_model, post_intervention_vae_model=None, T0=None, eps=None):
+def generate_example_sample(
+    data, vae_model, post_intervention_vae_model=None, T0=None, eps=None
+):
     # DATA -> PI -> Z -> W -> S(prime) ###
-    pi = vae_model.encode(torch.tensor(data).float())  # calculate vector in sampling space; pi = (mean, log variance)
+    pi = vae_model.encode(
+        torch.tensor(data).float()
+    )  # calculate vector in sampling space; pi = (mean, log variance)
     if eps is None:
         mu, logvar = pi
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
     Z = vae_model.reparameterize(pi, eps)  # drawing sample from the sampling space
 
-    S_init = torch.tensor(data[0:vae_model.lag, :, :]).float()  # initializing lag terms
+    S_init = torch.tensor(
+        data[0 : vae_model.lag, :, :]
+    ).float()  # initializing lag terms
 
     T = Z.shape[0]  # length of timeseries
     B = Z.shape[1]  # number of bundles
@@ -142,13 +159,17 @@ def generate_example_sample(data, vae_model, post_intervention_vae_model=None, T
         w_lag = torch.cat([w_appended] + [S[-i] for i in range(vae_model.lag)], dim=1)
 
         # POST INTERVENTION PART
-        if((post_intervention_vae_model is not None) and (T0 is not None)):
-            if t >= (T0-vae_model.lag):
-                if t == (T0-vae_model.lag):
+        if (post_intervention_vae_model is not None) and (T0 is not None):
+            if t >= (T0 - vae_model.lag):
+                if t == (T0 - vae_model.lag):
                     Sprime = S.clone()  # copy the pre-intervention model
-                sprime = post_intervention_vae_model.final_layer(w_lag)  # calculate the outcome
+                sprime = post_intervention_vae_model.final_layer(
+                    w_lag
+                )  # calculate the outcome
                 # collect the outcome
-                Sprime = torch.cat([Sprime, sprime.reshape([1, sprime.shape[0], sprime.shape[1]])])
+                Sprime = torch.cat(
+                    [Sprime, sprime.reshape([1, sprime.shape[0], sprime.shape[1]])]
+                )
 
         # PRE INTERVENTION PART/ COUNTERFACTUAL
         s = vae_model.final_layer(w_lag)
@@ -160,7 +181,7 @@ def generate_example_sample(data, vae_model, post_intervention_vae_model=None, T
         else:
             W = torch.cat([W, w.reshape([1, w.shape[0], w.shape[1]])])
 
-    if((post_intervention_vae_model is not None) and (T0 is not None)):
+    if (post_intervention_vae_model is not None) and (T0 is not None):
         return S, Sprime, T0, pi, Z, W
     return S, pi, Z, W
 
@@ -169,10 +190,13 @@ def generate_example_sample(data, vae_model, post_intervention_vae_model=None, T
 def plot(data, vae_model, post_intervention_vae_model=None, T0=None, out_folder=None):
     sns.set(font_scale=1.25)
 
-    if((post_intervention_vae_model is not None) and (T0 is not None)):
-        S, Sprime, T0, pi, Z, W = generate_example_sample(data, vae_model,
-                                                          post_intervention_vae_model=post_intervention_vae_model,
-                                                          T0=T0)
+    if (post_intervention_vae_model is not None) and (T0 is not None):
+        S, Sprime, T0, pi, Z, W = generate_example_sample(
+            data,
+            vae_model,
+            post_intervention_vae_model=post_intervention_vae_model,
+            T0=T0,
+        )
     else:
         S, pi, Z, W = generate_example_sample(data, vae_model)
     mu, logvar = pi
@@ -180,47 +204,64 @@ def plot(data, vae_model, post_intervention_vae_model=None, T0=None, out_folder=
     for i in range(data.shape[1]):
         fig, ax = plt.subplots(nrows=3, ncols=2, figsize=(35, 20))
 
-        ax[0, 0].plot(np.arange(0, T+vae_model.lag), (S).detach().numpy()[:, i, :], alpha=0.5)
-        ax[0, 0].set_ylabel('Normalized (Outcome)')
-        ax[0, 0].set_xlabel('Time Steps')
-        ax[0, 0].title.set_text('Simulated X')
+        ax[0, 0].plot(
+            np.arange(0, T + vae_model.lag), (S).detach().numpy()[:, i, :], alpha=0.5
+        )
+        ax[0, 0].set_ylabel("Normalized (Outcome)")
+        ax[0, 0].set_xlabel("Time Steps")
+        ax[0, 0].title.set_text("Simulated X")
 
-        ax[0, 1].plot(np.arange(0, T+vae_model.lag), data[:, i, :])
-        ax[0, 1].set_ylabel('Normalized (Outcome)')
-        ax[0, 1].set_xlabel('Time Steps')
-        ax[0, 1].title.set_text('Observed X')
+        ax[0, 1].plot(np.arange(0, T + vae_model.lag), data[:, i, :])
+        ax[0, 1].set_ylabel("Normalized (Outcome)")
+        ax[0, 1].set_xlabel("Time Steps")
+        ax[0, 1].title.set_text("Observed X")
 
-        ax[1, 0].plot(np.arange(vae_model.lag, vae_model.lag+T), (mu).detach().numpy()[:, i, :])
-        ax[1, 0].set_ylabel('Latent Value')
-        ax[1, 0].set_xlabel('Time Steps')
-        ax[1, 0].title.set_text('Mean Z')
+        ax[1, 0].plot(
+            np.arange(vae_model.lag, vae_model.lag + T), (mu).detach().numpy()[:, i, :]
+        )
+        ax[1, 0].set_ylabel("Latent Value")
+        ax[1, 0].set_xlabel("Time Steps")
+        ax[1, 0].title.set_text("Mean Z")
 
-        ax[1, 1].plot(np.arange(vae_model.lag, vae_model.lag+T), (logvar).detach().numpy()[:, i, :])
-        ax[1, 1].set_ylabel('Latent Value')
-        ax[1, 1].set_xlabel('Time Steps')
-        ax[1, 1].title.set_text('Log-Std Z')
+        ax[1, 1].plot(
+            np.arange(vae_model.lag, vae_model.lag + T),
+            (logvar).detach().numpy()[:, i, :],
+        )
+        ax[1, 1].set_ylabel("Latent Value")
+        ax[1, 1].set_xlabel("Time Steps")
+        ax[1, 1].title.set_text("Log-Std Z")
 
-        ax[2, 0].plot(np.arange(0, T+vae_model.lag),
-                      (S).detach().numpy()[:, i, :]-(S).detach().numpy()[:, i, :], alpha=0.5)
-        ax[2, 0].set_ylabel('Normalized (Outcome)')
-        ax[2, 0].set_xlabel('Time Steps')
-        ax[2, 0].title.set_text('Difference: X(Intervened) - X')
+        ax[2, 0].plot(
+            np.arange(0, T + vae_model.lag),
+            (S).detach().numpy()[:, i, :] - (S).detach().numpy()[:, i, :],
+            alpha=0.5,
+        )
+        ax[2, 0].set_ylabel("Normalized (Outcome)")
+        ax[2, 0].set_xlabel("Time Steps")
+        ax[2, 0].title.set_text("Difference: X(Intervened) - X")
 
-        ax[2, 1].plot(np.arange(vae_model.lag, T+vae_model.lag), (W).detach().numpy()[:, i, :])
-        ax[2, 1].set_ylabel('Outcome (Penultimate Layer)')
-        ax[2, 1].set_xlabel('Time Steps')
-        ax[2, 1].title.set_text('Simulated W')
+        ax[2, 1].plot(
+            np.arange(vae_model.lag, T + vae_model.lag), (W).detach().numpy()[:, i, :]
+        )
+        ax[2, 1].set_ylabel("Outcome (Penultimate Layer)")
+        ax[2, 1].set_xlabel("Time Steps")
+        ax[2, 1].title.set_text("Simulated W")
 
         if post_intervention_vae_model is not None:
             if T0 is not None:
                 ax[0, 0].set_prop_cycle(None)
-                ax[0, 0].plot(np.arange(0, T+post_intervention_vae_model.lag),
-                              (Sprime).detach().numpy()[:, i, :])
+                ax[0, 0].plot(
+                    np.arange(0, T + post_intervention_vae_model.lag),
+                    (Sprime).detach().numpy()[:, i, :],
+                )
                 ax[0, 0].axvline(x=T0)
                 ax[0, 0].set_prop_cycle(None)
-                ax[2, 0].plot(np.arange(0, T+post_intervention_vae_model.lag),
-                              (Sprime).detach().numpy()[:, i, :]-(S).detach().numpy()[:, i, :])
+                ax[2, 0].plot(
+                    np.arange(0, T + post_intervention_vae_model.lag),
+                    (Sprime).detach().numpy()[:, i, :] - (S).detach().numpy()[:, i, :],
+                )
                 ax[2, 0].axvline(x=T0)
 
         if out_folder is not None:
-            fig.savefig(out_folder+'example_sample_%d.png' % (i))
+            fig.savefig(out_folder + "example_sample_%d.png" % (i))
+
